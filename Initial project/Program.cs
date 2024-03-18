b36819079d71bd9a5f5b864172fc26d99c1de3d2
s22349 <s22349@pjwstk.edu.pl>
2024-03-18 13:32:12 +0100

    Intial project

---
+++ Initial project/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        // addStudent (Student stud){}
+        // AddStudent (Student stud){}
+        Console.WriteLine("Hello, World!");
+
+        int[] tbl = { 1, 2, 3, 4 };
+
+        foreach (int num in tbl)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
